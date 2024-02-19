f5f70a5740ceb7950272d4ce64bde18e4ada474d
LukaBch <lucas.banchereau@gmail.com>
2024-02-19 13:52:15 +0100

    Constraints.tsx updated

--- src/Constraints.tsx
+++ src/Constraints.tsx
@@ -16,9 +16,9 @@ function Constraints({
         <div style={{ display: 'flex', width: '100px', justifyContent: 'center' }}>
           {isFetchingResults ? '...' : (
             <>
-              <div>{donnees.find((donnee: any) => donnee.id === 1)!.energy}</div>
+              <div>{donnees.find((donnee: any) => donnee.id === 1)!.hours}</div>
               <div>+</div>
-              <div>{donnees.find((donnee: any) => donnee.id === 2)!.energy}</div>
+              <div>{donnees.find((donnee: any) => donnee.id === 2)!.hours}</div>
             </>
           )}
         </div>
@@ -32,11 +32,11 @@ function Constraints({
         <div style={{ display: 'flex', width: '100px', justifyContent: 'center' }}>
           {isFetchingResults ? '...' : (
             <>
-              <div>{donnees.find((donnee: any) => donnee.id === 3)!.energy}</div>
+              <div>{donnees.find((donnee: any) => donnee.id === 3)!.hours}</div>
               <div>+</div>
-              <div>{donnees.find((donnee: any) => donnee.id === 4)!.energy}</div>
+              <div>{donnees.find((donnee: any) => donnee.id === 4)!.hours}</div>
               <div>+</div>
-              <div>{donnees.find((donnee: any) => donnee.id === 5)!.energy}</div>
+              <div>{donnees.find((donnee: any) => donnee.id === 5)!.hours}</div>
             </>
           )}
         </div>
@@ -50,11 +50,11 @@ function Constraints({
         <div style={{ display: 'flex', width: '100px', justifyContent: 'center' }}>
           {isFetchingResults ? '...' : (
             <>
-              <div>{donnees.find((donnee: any) => donnee.id === 6)!.energy}</div>
+              <div>{donnees.find((donnee: any) => donnee.id === 6)!.hours}</div>
               <div>+</div>
-              <div>{donnees.find((donnee: any) => donnee.id === 7)!.energy}</div>
+              <div>{donnees.find((donnee: any) => donnee.id === 7)!.hours}</div>
               <div>+</div>
-              <div>{donnees.find((donnee: any) => donnee.id === 8)!.energy}</div>
+              <div>{donnees.find((donnee: any) => donnee.id === 8)!.hours}</div>
             </>
           )}
         </div>
